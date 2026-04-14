9211dea382f0c53b351c26d2fb640b6ebb045918
Arnold Jiang <aj9183@princeton.edu>
2026-04-14 16:55:01 -0400

    Modified minimal_run for toggling the search algorithm

--- src/minimal_run.py
+++ src/minimal_run.py
@@ -1,6 +1,7 @@
 import argparse
 from dotenv import load_dotenv
-from tot.methods.mcts import solve
+from tot.methods.bfs import solve as bfs_solve
+from tot.methods.mcts import solve as mcts_solve
 from tot.tasks.game24 import Game24Task
 
 load_dotenv()
@@ -9,10 +10,16 @@ args = argparse.Namespace(
     backend="openai/gpt-oss-120b",
     temperature=0.7,
     task="game24",
+    
     naive_run=False,
     prompt_sample=None,
     method_generate="propose",
     method_evaluate="value",
+    task_start_index=900,
+    task_end_index=901,
+    
+    # Modify the Search Algorithm
+    search_method="bfs",
 
     # BFS-only knobs; harmless if left in, but not used by MCTS
     method_select="greedy",
@@ -20,16 +27,15 @@ args = argparse.Namespace(
     n_evaluate_sample=3,
     n_select_sample=5,
 
-    # your index range
-    task_start_index=900,
-    task_end_index=901,
-
     # MCTS knobs
     n_mcts_simulations=500,
     mcts_exploration=1.4,
 )
 
 task = Game24Task()
-ys, info = solve(args, task, 900, to_print=False)
-for i in range(len(ys)):
-    print(ys[i] + "\n")
+solve = bfs_solve if args.search_method == "bfs" else mcts_solve
+
+# For debugging purposes (set to_print to True print all of ys): 
+res, info = solve(args, task, 900, to_print=False)
+solution = res[0] if res else ""
+print(solution)
